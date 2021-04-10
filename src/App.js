6035bb83e23de0a2eea158d2dc791fcf7230193e
Besmir Gjata <besigjata@gmail.com>
2021-04-10 14:28:35 +0200

    first part done

--- src/App.js
+++ src/App.js
@@ -1,2 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from'./components/ChatFeed'
+
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="71842685-053a-47f3-8a55-33d922fb73e8"
+      userName="jambo"
+      userSecret="123456"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+}
+
+
+export default App;
---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,21 @@
+const MyMessage = () => {
+  if(message?.attachments?.length > 0) {
+    return (
+      <img
+        src={message.attachments[0].file}
+        alt={message-attachment}
+        className={message-image}
+        style={{float: 'right'}}
+
+      />
+    )
+  }
+
+  return (
+    <div>
+      MyMessage
+    </div>
+  );
+}
+
+export default MyMessage;
